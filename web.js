96b35effc51e773fecc653b36d814729f5cafe85
Ubuntu <ubuntu@ip-172-31-36-77.us-west-2.compute.internal>
2013-07-10 18:31:16 +0000

    updating web.js

--- web.js
+++ web.js
@@ -1,14 +1,6 @@
-var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
+ fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
 });
