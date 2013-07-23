9b142005522aa0d9982365f64c59e00b67c11ec0
Britney Wright <blw06g@gmail.com>
2013-07-23 00:14:56 -0400

    update

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-
 var express = require('express');
-
-var app = express.createServer(express.logger());
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
- var fs = require('fs');
- var k = fs.readFileSync('index.html','utf-8');
- response.send(k);
+  response.send('index.html','utf-8');
 });
 
 var port = process.env.PORT || 5000;
